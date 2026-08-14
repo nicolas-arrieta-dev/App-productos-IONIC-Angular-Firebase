6b9fae7cbf569b0eaf6ca578d8010b6e3d6bbb11
Nicolas Alfonso Arrieta Bolaño <nicolas.arieta712@gmail.com>
2026-08-13 20:43:22 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-  apiKey: "AIzaSyASLhh37pZMXTbUe_CEAg7_tmAjoHfNBF4",
-  authDomain: "product-admin-app-82973.firebaseapp.com",
-  projectId: "product-admin-app-82973",
-  storageBucket: "product-admin-app-82973.firebasestorage.app",
-  messagingSenderId: "889537069666",
-  appId: "1:889537069666:web:0f42f0bc9a499d5105073b"
+  apiKey: "TUS_LLAVES",
+  authDomain: "TU DOMINIO",
+  projectId: "TU_PROYECT_ID",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
 }
 };
